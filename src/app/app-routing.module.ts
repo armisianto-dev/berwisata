910ac248972bcaf28104ce1ab56123a9f7f80175
armisianto-dev <armisianto@gmail.com>
2019-08-02 16:55:46 +0700

    Update account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,7 @@ const routes: Routes = [
   {
     path: 'account',
     loadChildren: './account/account.module#AccountModule',
+    canActivate: [AuthGuard],
     data: {
       state: 'account',
       title: 'Account | Berwisata',
